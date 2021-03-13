26a157db461e9d3aeb31c8043696b5c46048b183
Chitoku YATO <cyato@nvidia.com>
2021-03-13 02:30:33 -0800

    Adapt to the new model filename

--- pub/trtpose_handpose/pub_hand_msg_thread.py
+++ pub/trtpose_handpose/pub_hand_msg_thread.py
@@ -31,8 +31,8 @@ logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 HOST = "0.0.0.0" # Publish to Docker host
 PORT = "5001"
 
-ORG_MODEL = 'hand_pose_resnet18_att_244_244.pth'
-TRT_MODEL = 'hand_pose_resnet18_att_244_244_trt.pth'
+ORG_MODEL = 'handpose_resnet18_att_224x224_nvhand-2k6.pth'
+TRT_MODEL = 'handpose_resnet18_att_224x224_nvhand-2k6_trt.pth'
 
 # Creates a socket instance
 context = zmq.Context()
